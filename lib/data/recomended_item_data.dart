d33c14b43ed935dfbdbdc9b33a1f2547121932fd
foyzur17 <foyzur17@gmail.com>
2022-06-10 17:12:08 +0600

    add recomended Data

--- lib/data/recomended_item_data.dart
+++ lib/data/recomended_item_data.dart
@@ -3,16 +3,16 @@
 import 'package:first_e_commerse_app/model/recomended_data_model.dart';
 
 List<RecomendedItemModel> recomendedItemData =[
-  RecomendedItemModel(image: "assets/image/recomended1.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended2.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended3.png", title: "Nike Air Max 270 React Eng", price: "\$50,44"),
-  RecomendedItemModel(image: "assets/image/recomended4.png", title: "Nike Air Max 270 React Eng", price: "\$29,00"),
-  RecomendedItemModel(image: "assets/image/recomended1.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended2.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended3.png", title: "Nike Air Max 270 React Eng", price: "\$50,44"),
-  RecomendedItemModel(image: "assets/image/recomended4.png", title: "Nike Air Max 270 React Eng", price: "\$29,00"),
-  RecomendedItemModel(image: "assets/image/recomended1.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended2.png", title: "Nike Air Max 270 React Eng", price: "\$299,44"),
-  RecomendedItemModel(image: "assets/image/recomended3.png", title: "Nike Air Max 270 React Eng", price: "\$50,44"),
-  RecomendedItemModel(image: "assets/image/recomended4.png", title: "Nike Air Max 270 React Eng", price: "\$29,00"),
+  RecomendedItemModel(image: "assets/image/recomended1.png", title: "Lather Ledis Bag", price: "\$299"),
+  RecomendedItemModel(image: "assets/image/recomended2.png", title: "Nike Air Max 270 ", price: "\$780"),
+  RecomendedItemModel(image: "assets/image/recomended3.png", title: "Jens Sirt", price: "\$450"),
+  RecomendedItemModel(image: "assets/image/recomended4.png", title: "Gold nekles women", price: "\$30000"),
+  RecomendedItemModel(image: "assets/image/recomended5.png", title: "Men Watch ", price: "\$1200"),
+  RecomendedItemModel(image: "assets/image/recomended6.png", title: "Mens Panjabi Easy", price: "\$1800"),
+  RecomendedItemModel(image: "assets/image/recomended7.png", title: "Wairless Mouse", price: "\$999"),
+  RecomendedItemModel(image: "assets/image/recomended8.png", title: "Apple Laptop", price: "\$250000"),
+  RecomendedItemModel(image: "assets/image/recomended9.png", title: "Iphone 13 pro max", price: "\$12000"),
+  RecomendedItemModel(image: "assets/image/recomended10.png", title: "KTM Bike Indian", price: "\$42000"),
+  RecomendedItemModel(image: "assets/image/recomended11.png", title: "Gaming chair", price: "\$5500"),
+  RecomendedItemModel(image: "assets/image/recomended12.png", title: "DRLS Camera Nickon", price: "\$80000"),
 ];
